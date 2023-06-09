533a1be01e64b0bb0a7fae61f5b2a6f0293d3d2c
Dusya <Dusya@gmail.com>
2023-06-09 21:56:20 +0300

    add example 009 seminar_1

---
+++ Example009_sevinar_1/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая на вход принимает число и выдает его квадрат
+
+Console.Write("Введите число =>"); //Вывод приглашения на экран
+string imputedString = Console.ReadLine(); // Ввод пользователя (строка)
+int number = Convert.ToInt32(imputedString); // Преобразование строки в число
+Console.WriteLine("Вы ввели число " + number + "!"); // Вывод введенного значения
+Console.WriteLine($"Вы ввели число {number}!"); // Вывод введенного значения (другой вариант)
+
+int square = number * number; // Вычисление квадрата (логига программы)
+Console.WriteLine($"Квадрат числа {number} равен {square}");// Вывод результата
+
+
