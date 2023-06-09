ed7b744a18623f6d2f7ab9284d92d1a334f7e4bf
Dusya <Dusya@gmail.com>
2023-06-09 21:58:32 +0300

    add example 013 4 task

---
+++ Example013_4task/Program.cs
@@ -0,0 +1,17 @@
+﻿// Написать программу, которая на выход принимает одно число (N), а на выходе показывает все
+// целые числа в промежутке от -N до N
+
+Console.Write("Напишите целое положительное число "); //Вывод приглашения на экран
+
+string imputedString = Console.ReadLine(); // Ввод пользователя (строка)
+int number = Convert.ToInt32(imputedString); // Преобразование строки в число
+int number_2 = - number;
+
+while (number_2 <= number)
+    {
+    Console.Write($"{number_2}, ");
+    number_2++;
+    }
+
+
+
